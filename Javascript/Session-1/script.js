375b4472b9a50e1c3f7a0a4216924103acf1cde1
sonam-niit <sonamlovesmumypapa@gmail.com>
2023-07-15 19:28:28 +0530

    Javascript & variables

---
+++ Javascript/Session-1/script.js
@@ -0,0 +1,9 @@
+document.write("Addition: ")
+document.write("34+67= ");
+document.write(34+67);
+
+//to declare a variable we can use an inbuilt keyword called var
+
+//var var_name = value; //Syntax
+
+
